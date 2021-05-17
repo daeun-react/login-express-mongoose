97fa5a7d5db0fddb28082d7ab1283345c4a86633
daeun-react <67173064+daeun-react@users.noreply.github.com>
2021-05-17 14:02:47 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,20 +2,35 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const config = require("./config/key");
+const { User } = require("./models/User");
+
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://daeun:ekdms2954@react.u657q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected Success..."))
   .catch((err) => console.log("MongoDB Connected Error...", err));
 
-app.get("/", (req, res) => res.send("hello world!"));
+app.get("/", (req, res) => res.send("hello world!!!"));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`port ::: ${port}`));
